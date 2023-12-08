bf61f02cf9618781f8d9230abeb943c593ca563e
Nate St. George <nateestes223@gmail.com>
2023-12-08 11:22:42 -0700

    Improve docker volume usage

--- start-prod.sh
+++ start-prod.sh
@@ -1,5 +1,5 @@
-cp ~/backend-env ~/progressive-review/backend/.env
-cd ~/progressive-review
-docker system prune -f
+# cp ~/backend-env ~/progressive-review/backend/.env
+# cd ~/progressive-review
+# docker system prune -f
 docker-compose pull
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --build --force-recreate --detach
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --build --force-recreate # --detach
